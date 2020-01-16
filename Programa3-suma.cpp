d7b67671e9609793574c0a3464cca24146d8b139
kmtheo20 <59947029+kmtheo20@users.noreply.github.com>
2020-01-15 22:58:27 -0500

    Add files via upload

---
+++ Programa3-suma.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int main() 
+{
+  int A, B, C, res;
+  printf (" ingrese numero ");
+  scanf("%d",&A);
+  printf (" ingrese numero ");
+  scanf("%d",&B);
+  printf (" ingrese numero ");
+  scanf("%d",&C);
+  res=((A /(B + C) )/ A;
+  printf (" la respuesta es:%d",res);
+  getch();
+  return 0;
+}
