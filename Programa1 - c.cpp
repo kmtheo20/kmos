f28d9e7ead8838fe3a1e5e24c03df16faa3a7352
kmtheo20 <59947029+kmtheo20@users.noreply.github.com>
2020-01-15 23:10:42 -0500

    Este es un archivo reciente subido

---
+++ Programa1 - c.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+    int num; 
+    printf ("Ingrese un numero\n");
+    scanf ("%d",&num);
+    printf("el numero que ingresaste es: %d \n",num);
+    getchar (); 
+    return 0; 
+}
